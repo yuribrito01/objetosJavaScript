0d9ef64f413aaee82e0b842e25824d202da2508d
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-07 10:02:00 -0300

    Criando um codigo para exibir informações do cliente

---
+++ desafio1.js
@@ -0,0 +1,13 @@
+//acessar um objeto com informações de um cliente, e exibir essas informações no console
+
+const cliente ={
+    nome:"Yuri",
+    idade:20,
+    cpf: 123456789
+};
+
+//console.log(cliente);
+
+//melhorando!
+
+console.log(`O nome do cliente é ${cliente.nome}, possui ${cliente.idade} anos e é portador(a) do CPF: ${cliente.cpf}`);
