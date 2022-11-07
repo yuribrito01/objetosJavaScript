5f3b806c26f7fa273287f32cec03fe3f6005b38d
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-07 12:16:30 -0300

    Acessando mais de um numero de telefone

--- acessarTelefones.js
+++ acessarTelefones.js
@@ -3,5 +3,7 @@ const cliente ={
     idade:20,
     cpf: '123456789',
     email:"yuri@email.com",
-    phone: "707070"
+    phone: ["707070","123445"]
 };
+
+cliente.phone.forEach(phone => console.log(phone));
