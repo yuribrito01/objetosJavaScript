f3f5616ea8344656417a5dc1151327b5ecbc27c2
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-11-08 09:05:55 -0300

    Criando um objeto dentro de outro objeto

---
+++ objetosEmObjetos.js
@@ -0,0 +1,16 @@
+const cliente ={
+    nome:"Yuri",
+    idade:20,
+    cpf: '123456789',
+    email:"yuri@email.com",
+    phone: ["707070","123445"]
+};
+
+cliente.dependente = {
+    nome:"Yara",
+    parentesco:"Irmã",
+    idade: 20
+
+}
+
+console.log(cliente);
